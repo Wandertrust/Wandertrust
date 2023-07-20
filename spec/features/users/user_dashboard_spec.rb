b60bec1270f04e08f04c4cae0e85b6be0bdca79d
Tori Enyart <torienyart@gmail.com>
2023-07-20 12:54:16 -0600

    Start writing tests for user dashboard

---
+++ spec/features/users/user_dashboard_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'as a logged in user' do
+  it 'I see a list of recently created activities' do
+    #could eventually create a filter that pulls the activities either by date created or date updated or start date of the activity
+
+  end
+
+  it "I see a button to track a new activity" do
+
+  end
+
+  it "I see some sort of trend representation to see how many miles I have done or ft elevation gain etc..." do
+    
+  end
+end
