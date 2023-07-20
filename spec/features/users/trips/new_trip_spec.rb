f2ed7cd180025176e56a1d05e1198507a9781c81
Tori Enyart <torienyart@gmail.com>
2023-07-20 15:14:05 -0600

    Get basic page layout completed

--- spec/features/users/trips/new_trip_spec.rb
+++ spec/features/users/trips/new_trip_spec.rb
@@ -9,22 +9,21 @@ describe "as a logged in user" do
       fill_in 'password', with: '1234'
       click_on "Let's Wander"
     end
+    visit new_user_trip_path(@user.id)
   end
 
   it "i can access a page to create a new trip that includes fields for name, start and end time, gpx_file, and inviting other users" do
-    visit new_user_trip_path(@user.id)
-
     expect(page).to have_field('Name')
     expect(page).to have_field('Start Time')
     expect(page).to have_field('Projected End Time')
-    expect(page).to have_field('gpx file')
-    expect(page).to have_select('Invite other users')
+    expect(page).to have_field('gpx_file')
+    # expect(page).to have_select('Invite other users')
   end
 
   it "the gpx can also be pulled from strava or ride with gps" do
     within "#3rd-party-gpx" do
-      expect(page).to have_button('Strava')
-      expect(page).to have_button('Ride with GPS')
+      expect(page).to have_button('Connect a Strava Route')
+      expect(page).to have_button('Connect a Ride With GPS Route')
     end
   end
 
