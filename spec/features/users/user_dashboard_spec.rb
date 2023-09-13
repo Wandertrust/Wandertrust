1b24bb914cff55dcdce9f55341ae7b6eca6c3eea
Tori Enyart <torienyart@gmail.com>
2023-09-13 16:06:16 -0600

    Add testing for additional trip information and dashboard buttons

--- spec/features/users/user_dashboard_spec.rb
+++ spec/features/users/user_dashboard_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe 'as a logged in user' do
   before :each do
     @user = create(:user)
-    @user.trips.create(name: 'Ward Loop', start_time: "10/18/22 9:00AM", end_time: "10/18/22 2:00PM", gpx_file: 'ward_loop.gpx')
-    @user.trips.create(name: 'La Sal Loop', start_time: "10/18/23 9:00AM", end_time: "10/18/23 2:00PM", gpx_file: 'la_sal_loop.gpx')
-    @user.trips.create(name: 'Skyline', start_time: DateTime.now, end_time: DateTime.now + 1, gpx_file: 'skyline.gpx')
+    @trip_1 = @user.trips.create(name: 'Ward Loop', start_time: "10/18/22 9:00AM", end_time: "10/18/22 2:00PM", gpx_file: 'ward_loop.gpx')
+    @trip_2 = @user.trips.create(name: 'La Sal Loop', start_time: "10/18/23 9:00AM", end_time: "10/18/23 2:00PM", gpx_file: 'la_sal_loop.gpx')
+    @trip_3 = @user.trips.create(name: 'Skyline', start_time: DateTime.now, end_time: DateTime.now + 1, gpx_file: 'skyline.gpx')
 
     visit login_path
     fill_in 'email', with: @user.email
@@ -19,7 +19,16 @@ describe 'as a logged in user' do
     within "#upcoming-trips" do
       expect(page).to have_content('La Sal Loop')
       expect(page).to have_content('Skyline')
-      # expect(page).to have_no_content('Ward Loop')
+      expect(page).to have_no_content('Ward Loop')
+    end
+  end
+
+  it 'each listed activity has a link to view the activity and brief information' do
+    within "#upcoming-trips" do
+      within "#trip-#{@trip_2.id}" do
+        expect(page).to have_link('View Trip')
+        expect(page).to have_content(@trip_1.start_time.strftime("%A, %B %d, %Y"))
+      end
     end
   end
 
@@ -28,7 +37,7 @@ describe 'as a logged in user' do
   end
 
   xit "I see a button to view old trips" do
-
+    expect(page).to have_button('Review old trips')
   end
 
   xit "I see some sort of trend representation to see how many miles I have done or ft elevation gain etc..." do
