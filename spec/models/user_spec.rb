b694758406a945bf5ff4b7c8f64c36dfb08a7e5e
Tori Enyart <torienyart@gmail.com>
2023-09-19 14:27:16 -0600

    Add testing for user model method to filter user trips by past and upcoming/current

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :password }
   end
 
+  describe 'instance methods' do
+    before :each do
+      @user = create(:user)
+      @trip_1 = @user.trips.create(name: 'Ward Loop', start_time: "10/18/22 9:00AM", end_time: "10/18/22 2:00PM", gpx_file: 'ward_loop.gpx')
+      @trip_2 = @user.trips.create(name: 'La Sal Loop', start_time: DateTime.now - 1, end_time: DateTime.now + 1, gpx_file: 'la_sal_loop.gpx')
+      @trip_3 = @user.trips.create(name: 'Skyline', start_time: DateTime.now + 1, end_time: DateTime.now + 2, gpx_file: 'skyline.gpx')
+    end
+
+    it '#upcoming_trips' do
+      expect(@user.upcoming_trips).to eq([@trip_2, @trip_3])
+      expect(@user.upcoming_trips).to_not include(@trip_1)
+    end
+  end
 end
